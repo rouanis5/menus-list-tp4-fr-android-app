3955743c0a90f553ff1d47c874a1462d03c553cc
rouanis5 <ouanis42@gmail.com>
2022-12-29 23:29:47 +0100

    better code on liste_simple.java

--- app/src/main/java/com/example/menus_list_tp4_fr_android_app/liste_simple.java
+++ app/src/main/java/com/example/menus_list_tp4_fr_android_app/liste_simple.java
@@ -55,54 +55,40 @@ public class liste_simple extends AppCompatActivity{
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
         ListView lv = findViewById(R.id.mysimplelist);
 
-        Dialog dialog = new Dialog(this);
-        dialog.setContentView(R.layout.dialog_simple);
-
-        EditText et = dialog.findViewById(R.id.dialog_edittext);
-        Button validBtn = dialog.findViewById(R.id.dialog_valider),
-                annulBtn = dialog.findViewById(R.id.dialog_annuler);
         switch (item.getItemId()) {
             case R.id.delete_item:
                 list.remove(info.position);
                 setArrayAdapter(lv);
                 break;
             case R.id.add_item:
-                ((TextView) dialog.findViewById(R.id.dialog_title)).setText("nouveau étudiant: ");
+                Dialog addingDialog = showDialog("nouveau étudiant", "");
+                Button validAddingDialogBtn = addingDialog.findViewById(R.id.dialog_valider);
+                EditText etAddingDialog = addingDialog.findViewById(R.id.dialog_edittext);
 
-                dialog.show();
-
-                validBtn.setOnClickListener(v -> {
-                    String name = et.getText().toString();
+                validAddingDialogBtn.setOnClickListener(v -> {
+                    String name = etAddingDialog.getText().toString();
                     list.add(name);
-                    Toast.makeText(this,name+" is added !", Toast.LENGTH_SHORT).show();
+//                    Toast.makeText(this,oldName+" is updated to "+name+" !", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this, name + " est ajouté(e) à la liste !", Toast.LENGTH_SHORT).show();
                     setArrayAdapter(lv);
-                    dialog.dismiss();
-                });
-
-                annulBtn.setOnClickListener(v -> {
-                    dialog.dismiss();
+                    addingDialog.dismiss();
                 });
                 break;
             case R.id.edit_item:
                 Integer id = Math.toIntExact(info.id);
                 String oldName = list.get(id);
 
-                ((TextView) dialog.findViewById(R.id.dialog_title)).setText("edit student "+ oldName);
-                et.setText(oldName);
+                Dialog editDialog = showDialog("modifier l'étudiant " + oldName, oldName);
+                Button validEditDialogBtn = editDialog.findViewById(R.id.dialog_valider);
+                EditText etEditDialog = editDialog.findViewById(R.id.dialog_edittext);
 
-                dialog.show();
-
-                validBtn.setOnClickListener(v -> {
-                    String name = et.getText().toString();
+                validEditDialogBtn.setOnClickListener(v -> {
+                    String name = etEditDialog.getText().toString();
                     list.remove(info.position);
                     list.add(id, name);
-                    Toast.makeText(this,oldName+" is updated to "+name+" !", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(this,oldName+" est devenu "+name+" !", Toast.LENGTH_SHORT).show();
                     setArrayAdapter(lv);
-                    dialog.dismiss();
-                });
-
-                annulBtn.setOnClickListener(v -> {
-                    dialog.dismiss();
+                    editDialog.dismiss();
                 });
                 break;
 
@@ -117,5 +103,23 @@ public class liste_simple extends AppCompatActivity{
          ArrayAdapter<String> a = new ArrayAdapter(
                  this, R.layout.simpleitem, R.id.text1, list);
          lv.setAdapter(a);
+     }
+
+     Dialog showDialog(String Title, String edittext_content){
+         Dialog dialog = new Dialog(this);
+         dialog.setContentView(R.layout.dialog_simple);
+
+         EditText et = dialog.findViewById(R.id.dialog_edittext);
+         Button annulBtn = dialog.findViewById(R.id.dialog_annuler);
+
+         ((TextView) dialog.findViewById(R.id.dialog_title)).setText(Title);
+         et.setText(edittext_content);
+
+         dialog.show();
+
+         annulBtn.setOnClickListener(v -> {
+             dialog.dismiss();
+         });
+         return dialog;
      }
 }
